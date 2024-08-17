ca010a905b3fd04d1f755df1844f80e2b111b3e0
zysoong <ziyang.song@gmx.de>
2024-08-17 21:45:53 +0200

    Establish environment and example script

--- browser_with_retrieval_example.py
+++ browser_with_retrieval_example.py
@@ -15,7 +15,7 @@ from langchain_core.tools import BaseTool
 from langchain_core.utils.function_calling import convert_to_openai_tool
 from langchain_openai import ChatOpenAI
 
-from tool.toolkit import RetrievalPlayWrightBrowserToolkit
+from langchain_community.agent_toolkits.playwright.toolkit import RetrievalPlayWrightBrowserToolkit
 
 
 def create_openai_retrieval_tools_agent(llm: BaseLanguageModel, tools: Sequence[BaseTool], prompt: ChatPromptTemplate) -> Runnable:
