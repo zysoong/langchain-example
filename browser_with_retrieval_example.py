a4c8c73fb0bfd4c61eace70245d7d0077e571f7c
zysoong <ziyang.song@gmx.de>
2024-08-18 15:03:19 +0200

    Update langchain

--- browser_with_retrieval_example.py
+++ browser_with_retrieval_example.py
@@ -1,4 +1,4 @@
-from langchain.agents.openai_tools.base import create_openai_retrieval_tools_agent
+from langchain.agents.openai_tools.base import create_openai_tools_agent_and_inject_prompts
 from langchain_community.tools.playwright.utils import (
     create_sync_playwright_browser
 )
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     tools = toolkit.get_tools()
     llm = ChatOpenAI(model="gpt-4", temperature=0)
 
-    agent = create_openai_retrieval_tools_agent(llm, tools, prompt)
+    agent = create_openai_tools_agent_and_inject_prompts(llm, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     command = {
         "quest": "On_Rough_Seas"
